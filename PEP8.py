15c74038dbdea3b8444d3ce32e177fd910acc146
Renan Arimatea <renanmoraiz@gmail.com>
2022-03-21 22:45:32 -0300

    Analisando conceitos de PEP 8

---
+++ PEP8.py
@@ -0,0 +1,30 @@
+"""
+PEP 8 - Index if Python Enhancement Proposals (Indices de Propostas de Aprimoramento Python)
+"""
+
+"""
+O que são PEP's
+
+- São arquivos que apresentam propostas de melhorias para a Linguagem Python.
+
+- https://peps.python.org/
+
+Regras mais utilizadas:
+
+1) O pycharm vem com padrão de reconhecimento de palavras em inglês.
+- Para o Pycharm reconhecer palavras em português faça:
+    File - settings - Inspections - Proofreading - Desmarque Type
+
+2) Identação (organizar o código):
+- Utilize 4 espaços para cada código dentro de blocos como funções, condicionais, classes, dentre outros.
+    #Ex (Condicionais):
+        if x < 10:
+            if y > 10:
+                y = 5
+                
+3) Linhas em branco:
+- Seu programa deve haver sempre 01 linha em branco no final;
+- Separe funções e classes com 02 linhas em branco.
+"""
+
+print('Hello World!')

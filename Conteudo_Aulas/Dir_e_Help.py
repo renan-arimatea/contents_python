8d1e15666e22283d1c09d11e0e0463e0dd50f245
Renan Arimatea <renanmoraiz@gmail.com>
2022-03-23 21:23:34 -0300

    Estudando conceitos de ajuda e suporte com Dir e Help

---
+++ Conteudo_Aulas/Dir_e_Help.py
@@ -0,0 +1,22 @@
+"""
+
+Dir e Help
+
+Dir: Apresenta uma lista de funções e métodos que podem ser utilizados em algum tipo de dado.
+
+Sintaxe: dir(valor / tipo de dado)
+
+Help: Apresenta as intruções de utilização das funções e métodos disponíveis para determindado tipo de dado.
+
+Sintaxe: help(função) ou help( valor / tipo de dado.método/função)
+
+"""
+
+# Dir
+print(dir("urso"))
+print(dir(18))
+print(dir(float))
+
+# Help
+print(help(print))
+print(help("Renan".title))

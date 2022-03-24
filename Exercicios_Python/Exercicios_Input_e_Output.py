65f1ef43f08712c435706ece38157b47d37b101b
Renan Arimatea <renanmoraiz@gmail.com>
2022-03-23 21:04:10 -0300

    Realizado os exercícios da aula de Input e Output e organizado as pastas do curso.

---
+++ Exercicios_Python/Exercicios_Input_e_Output.py
@@ -0,0 +1,15 @@
+"""
+Exercícios:
+
+1 - Imprimir na tela a mensagem: 'Olá Mundo'.
+2 - Receber nome e idade do usuário e imprimi-los em um único print.
+
+"""
+# Resposta 01
+print("Olá Mundo")
+
+# Resposta 02
+nome = input("Qual é o seu nome: ")
+idade = input("Qual é a sua idade: ")
+
+print(f"Seu nome é {nome} e sua idade é {idade} anos. ")
